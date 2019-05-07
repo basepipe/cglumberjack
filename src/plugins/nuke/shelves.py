8a83c124ec886e391ff8e264096922888d6f34d4
tmikota <tmikota@gmail.com>
2019-05-07 11:39:03 -0400

    Got parsing of project xmls working.  now i need to work on creating more complete project xmls so that i am reading more pertinant data

--- src/plugins/nuke/shelves.py
+++ src/plugins/nuke/shelves.py
@@ -6,6 +6,7 @@ from cglcore.path import PathObject
 
 
 PATH_OBJECT = PathObject(str(nuke.root().name()))
+# TODO = I need a way of figuring out whether a PATH OBJECT is in compliance with the pipeline
 COMPANY_CONFIG = os.path.dirname(PATH_OBJECT.company_config.replace('/', '\\'))
 NUKE_SHELVES_PATH = os.path.join(COMPANY_CONFIG, 'cgl_tools', 'nuke', 'shelves.yaml')
 NUKE_SHELVES = os.path.join(os.path.dirname(NUKE_SHELVES_PATH), 'shelves')
