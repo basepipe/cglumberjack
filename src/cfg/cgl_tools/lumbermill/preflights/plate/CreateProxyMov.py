add4749a11af710825626e23583c0338a6ffad9f
tmikota <tmikota@gmail.com>
2019-07-09 09:41:17 -0400

    simplified the company creation with a flag that allows me to ONLY create companies within the default project management structure.

--- src/cfg/cgl_tools/lumbermill/preflights/plate/CreateProxyMov.py
+++ src/cfg/cgl_tools/lumbermill/preflights/plate/CreateProxyMov.py
@@ -2,7 +2,7 @@ import glob
 import os
 from plugins.preflight.preflight_check import PreflightCheck
 from cglcore.path import PathObject, lj_list_dir
-from cglcore.convert import create_mov_hd_proxy
+from cglcore.convert import create_mov
 
 
 class CreateProxyMov(PreflightCheck):
@@ -14,7 +14,7 @@ class CreateProxyMov(PreflightCheck):
         # get the sequence to be converted
         frange = self.shared_data['frange']
         sframe, eframe = frange.split('-')
-        create_mov_hd_proxy(self.shared_data['hdProxy'], start_frame=sframe)
+        create_mov(self.shared_data['hdProxy'], start_frame=sframe)
         self.pass_check('Movie Created!')
         
         
