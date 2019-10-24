9a7679ebeb5e97ba0b1877f6b56d020e27ff2c79
tmikota <tmikota@gmail.com>
2019-10-24 12:05:31 -0400

    added latest .bat scripts to the repo

---
+++ resources/bat_scripts/python_setup.bat
@@ -0,0 +1,9 @@
+SETX /M PYTHONPATH "Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\latest\cglumberjack\src;Z:\COMPANIES\_config"
+SETX /M PATH "%PATH%;Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\latest\cglumberjack\src"
+choco install git -y
+choco install python2 -y
+choco install ffmpeg -y
+choco install imagemagick -y
+choco install wget -y
+
+
