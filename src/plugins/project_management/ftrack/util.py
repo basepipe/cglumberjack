c4459694fcec1a95b15ff43962a8ae8338b6a573
km16f <km16f@my.fsu.edu>
2019-10-29 14:23:09 -0400

    Implemented function to get tasks for a specific project in ftrack

--- src/plugins/project_management/ftrack/util.py
+++ src/plugins/project_management/ftrack/util.py
@@ -5,20 +5,35 @@ server_url = app_config()['project_management']['ftrack']['api']['server_url']
 api_key = app_config()['project_management']['ftrack']['api']['api_key']
 api_user = app_config()['project_management']['ftrack']['api']['api_user']
 
+ftrack = ftrack_api.Session(server_url=server_url, api_key=api_key, api_user=api_user)
 
-def get_all_projects(server_url):
+
+def get_all_projects():
     """
-    Function to get a list of all projects found in Ftrack at a given server
+    Function to get a list of all projects for a company in ftrack
 
-    :param server_url: A URL for a company in FTrack
-    :type server_url: String
     :return: Returns a list of Project Objects
     """
-    ftrack = ftrack_api.Session(server_url=server_url, api_key=api_key, api_user=api_user)
     proj = ftrack.query("Project")
+    # for p in proj:
+    #     print p['name']
     return proj
 
 
-p = get_all_projects(server_url)
-for proj in p:
-    print proj['name']
+def get_all_tasks(proj_name):
+    """
+    Function to get a list of all tasks for a project in ftrack
+
+    :param proj_name: Name of Project to get info from
+    :type proj_name: String
+    :return: List of Task Objects
+    """
+    t = ftrack.query("Task where project.name is '%s'" % proj_name)
+    # for task in t:
+    #     print task['name']
+    return t
+
+
+project = get_all_projects()
+tasks = get_all_tasks("cgl_testProjectA")
+
