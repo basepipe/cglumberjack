818b2dfb45e14ad931e070f5145db977fcbc3359
tmikota <tmikota@gmail.com>
2019-11-08 21:53:57 -0500

    changed out basically all the shutil functions with cgl_copy, cgl_copy make basically everything faster in the app.

--- src/cglcore/example_code/copyTest.py
+++ src/cglcore/example_code/copyTest.py
@@ -54,7 +54,7 @@ def robocopy_test(from_path, to_path):
     # robocopy command - all these flags are needed to avoid printouts and save time.
     command = 'robocopy "%s" "%s" /NFL /NDL /NJH /NJS /nc /ns /np /MT:8' % (from_path, to_path)
     start_time = time.time()
-    os.system(command)
+    # os.system(command)
     print 'Copied %s files with robocopy in %s' % (len(files), (time.time() - start_time))
     # for f in files:
     #     to_file = os.path.join(to_dir, f)
