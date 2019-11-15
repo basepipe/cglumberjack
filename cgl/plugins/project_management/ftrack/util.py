0ce9850b2affe5144160c6480d61d3e3b68d25e0
km16f <km16f@my.fsu.edu>
2019-11-15 16:28:42 -0500

    Added two new columns to TimeTracker Implemented get_all_time function for FTrack Task

--- cgl/plugins/project_management/ftrack/util.py
+++ cgl/plugins/project_management/ftrack/util.py
@@ -62,6 +62,15 @@ def get_timelogs(month=datetime.datetime.today().month, day=datetime.datetime.to
     return timelogs
 
 
+def get_total_time(task):
+    total = 0
+    timelogs = task['timelogs']
+    for log in timelogs:
+        total += log['duration']
+    print total/60/60
+    return total/60/60
+
+
 def edit_timelog(timelog_id, hours):
     """
     Function to edit pre-existing timelog in ftrack
@@ -153,3 +162,8 @@ def get_all_tasks(proj_name):
 if __name__ == '__main__':
     t = get_all_tasks('cgl_testProjectA')
     test = get_timelogs(datetime.datetime.today().month, datetime.datetime.today().day, datetime.datetime.today().year)
+    ftrack = setup()
+    result = ftrack.query("Task where id is dfb1ba9a-302d-4761-a719-77fe07cf000f")
+    task = result[0]
+    get_total_time(task)
+
