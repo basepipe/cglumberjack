34c0e57fa36a8a4634747a99f23219172a285ac4
tmikota <tmikota@gmail.com>
2019-12-10 17:32:01 -0500

    1152882011247561 Create a PySide GUI that displays the text from the json file created in this tr...

--- cgl/apps/robo_gary/main.py
+++ cgl/apps/robo_gary/main.py
@@ -15,33 +15,68 @@ class RoboGary(LJDialog):
         self.title_line_edit = QtWidgets.QLineEdit()
         self.title_line_edit.setText('TitleOfInterview')
         self.script_text_area = QtWidgets.QPlainTextEdit()
+        self.raw_text_area = QtWidgets.QPlainTextEdit()
+        self.width = 600
+        self.script_text_area.setMaximumWidth(self.width)
         self.script_layout.addWidget(self.title_line_edit)
         self.script_layout.addWidget(self.script_text_area)
 
         trifecta_layout.addLayout(self.script_layout)
+        trifecta_layout.addWidget(self.raw_text_area)
         self.load_transcript()
 
     def load_transcript(self):
         transcript_file = r'B:\Users\tmiko\Downloads\tom_ahmed_conversation_12_10_2019.json'
         transcript = load_json(transcript_file)
         # for now assumes 1 transcript
-        print transcript.keys()
         words = transcript['results']['items']
-        for w in words:
-            start_time = ''
-            end_time = ''
-            if 'start_time' in w.keys():
-                start_time = w['start_time']
-            if 'end_time' in w.keys():
-                end_time = w['end_time']
-            print w['alternatives'][0]['content'], start_time, end_time
         raw_text = transcript['results']['transcripts'][0]['transcript']
         speaker_labels = transcript['results']['speaker_labels']
-        self.script_text_area.setPlainText(raw_text)
+        self.raw_text_area.setPlainText(raw_text)
+        formatted_text = ""
         num_speakers = speaker_labels['speakers']
-        print len(speaker_labels['segments']), 'speech segments'
+        previous_speaker = ''
         for segment in speaker_labels['segments']:
-            print segment['start_time'], segment['end_time']
+            # find out if we're doing the same speaker or a new one:
+            speaker = segment['speaker_label']
+            if speaker != previous_speaker:
+                if previous_speaker:
+                    formatted_text = '%s\n\n%s\n%s' % (formatted_text, previous_speaker.upper().center(self.width/7),
+                                                       string_from_segment_list(previous_list))
+                segment_list = []
+            else:
+                segment_list = previous_list
+            start_time = segment['start_time']
+            end_time = segment['end_time']
+            segment_name = '%s_%s_%s' % (speaker, start_time, end_time)
+            for w in words:
+                # find the start and end index for the 'segment'
+                if 'start_time' in w.keys():
+                    if w['start_time'] >= segment['start_time']:
+                        in_segment = True
+                        if w['start_time'] < segment['end_time']:
+                            segment_list.append(w['alternatives'][0]['content'])
+                        if w['start_time'] > segment['end_time']:
+                            in_segment = False
+                else:
+                    if in_segment:
+                        segment_list.append(w['alternatives'][0]['content'])
+
+            previous_speaker = speaker
+            previous_list = segment_list
+        self.script_text_area.setPlainText(formatted_text)
+
+
+def string_from_segment_list(segment_list):
+    seg_string = ''
+    for s in segment_list:
+        if s == ',':
+            seg_string = '%s%s' % (seg_string, s)
+        elif s == '.':
+            seg_string = '%s%s  ' % (seg_string, s)
+        else:
+            seg_string = '%s %s' % (seg_string, s)
+    return seg_string
 
 
 if __name__ == "__main__":
