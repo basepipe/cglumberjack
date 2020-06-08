cf0a76dfe4bcc54065b3724fb57abf7d75fbc9a3
Tom Mikota <tmikota@gmail.com>
2020-06-08 16:41:49 -0400

    Updated bat scripts and updated files for installation.

--- resources/bat_scripts/step_one.bat
+++
@@ -1,7 +0,0 @@
-xcopy Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\install_chocolatey.bat "%userprofile%\Desktop" 
-xcopy Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\lumbermill.bat "%userprofile%\Desktop" 
-xcopy Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\python_requirements.bat "%userprofile%\Desktop"
-xcopy Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\python_setup.bat "%userprofile%\Desktop"
-xcopy /s Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\copy_user_globals_from_here "%userprofile%\Documents"
-setx PYTHONPATH=Z:\02_SYSTEMS\02_SystemConfig\CG_Lumberjack\latest\cglumberjack\src;
-setx PATH=%PYTHONPATH%;%PATH%;
