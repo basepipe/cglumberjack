291f3649d2b8e3020117a904f0c2aab4d86ff5c2
km16f <km16f@my.fsu.edu>
2020-06-24 16:37:05 -0400

    Implemented functions to autodoc an entire directory

--- docs/source/main.rst
+++ docs/source/main.rst
@@ -1,11 +1,67 @@
-AssetCore
-=======================
+Core
++++++++++++++++++++++++
 
+assetcore
+========================
 .. automodule:: core.assetcore
-    :members:
+	:members:
 
-Convert
-=======================
 
+cgl_convert
+========================
+.. automodule:: core.cgl_convert
+	:members:
+
+
+cgl_info
+========================
+.. automodule:: core.cgl_info
+	:members:
+
+
+config
+========================
+.. automodule:: core.config
+	:members:
+
+
+convert
+========================
 .. automodule:: core.convert
-    :members:
+	:members:
+
+
+lj_mail
+========================
+.. automodule:: core.lj_mail
+	:members:
+
+
+metadata
+========================
+.. automodule:: core.metadata
+	:members:
+
+
+path
+========================
+.. automodule:: core.path
+	:members:
+
+
+project
+========================
+.. automodule:: core.project
+	:members:
+
+
+screen_grab
+========================
+.. automodule:: core.screen_grab
+	:members:
+
+
+startup
+========================
+.. automodule:: core.startup
+	:members:
--- docs/utils.py
+++ docs/utils.py
@@ -7,10 +7,15 @@ from core.config import app_config
 from core.utils.general import cgl_execute
 
 CONFIG = app_config()
-html_root = os.path.join(CONFIG['paths']['code_root'], 'docs', 'build', 'html')
+code_root = os.path.join(CONFIG['paths']['code_root'])
+html_root = os.path.join(code_root, 'docs', 'build', 'html')
 
 
 def setup_path():
+    """
+    Sets up path object so sphinx can read cgl modules
+    :return:
+    """
     root_ = CONFIG['paths']['code_root']
     sys.path.append(root_)
     sys.path.append(os.path.join(root_, 'cgl'))
@@ -18,17 +23,83 @@ def setup_path():
 
 
 def make_build():
+    """
+    Cleans and rebuilds documentation html file
+    :return:
+    """
     os.chdir(os.path.dirname(__file__))
     c = subprocess.call(['make', 'clean'], shell=True)
     p = subprocess.call(['make', 'html'], shell=True)
 
 
 def open_html(file_name):
+    """
+    Opens index.html file in google chrome
+    :param file_name: Path to html file
+    :return:
+    """
     url = file_name
     webbrowser.open(url)
 
 
+def get_current_documented_modules(rst_name):
+    """
+    Function to get list of modules already listed in autodoc
+     --- Modules MUST follow an automodule statement
+    :param rst_name: Name of rst file containing auto documented modules
+    :return: List of module names that have been documented
+    """
+    rst_path = os.path.join(code_root, 'docs', 'source', rst_name)
+    module_list = []
+    with open(rst_path) as text_file:
+        lines = text_file.readlines()
+        for line in lines:
+            if "automodule::" in line:
+                module = line.split('.')
+                module_list.append(module[3].rstrip('\n'))
+    return module_list
+
+
+def get_directory_modules(dir_name):
+    """
+    Finds all python modules located in directory
+    :param dir_name: Name of directory to search through
+    :return: List of module names
+    """
+    return_list = []
+    dir_path = os.path.join(code_root, 'cgl', dir_name)
+    if os.path.isdir(dir_path):
+        for file in os.listdir(dir_path):
+            if file != "__init__.py":
+                if '.py' in file and '.pyc' not in file:
+                    file_split = file.split('.')
+                    return_list.append(file_split[0])
+    return return_list
+
+
+def add_module_to_rst(rst_name, dir_name, module_name):
+    rst_path = os.path.join(code_root, 'docs', 'source', rst_name)
+    with open(rst_path, 'a') as f:
+        f.write('\n\n%s\n========================' % module_name)
+        f.write('\n.. automodule:: %s.%s\n' % (dir_name, module_name))
+        f.write('\t:members:\n')
+
+
+def document_directory(rst_name, dir_name):
+    """
+    Iterates through directory and autodocs each module found
+    :param rst_name: Name of rst file to add modules to
+    :param dir_name: Name of directory to iterate through
+    :return:
+    """
+    doc_list = get_current_documented_modules(rst_name)
+    dir_list = get_directory_modules(dir_name)
+    for dir in dir_list:
+        if dir in doc_list:
+            pass
+        else:
+            add_module_to_rst(rst_name, dir_name, dir)
+
+
 if __name__ == '__main__':
-    make_build()
-    html_file = os.path.join(html_root, 'index.html')
-    open_html(html_file)
+    pass
