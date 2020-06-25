c4aee2e5b5c4a8d4dd0e98dfc54e786518c98a9a
Tom Mikota <tmikota@gmail.com>
2020-06-25 17:45:30 -0400

    Added my default maya scripts for basic shelf and preflights.

---
+++ cgl/cfg/cgl_tools/maya/preflights/mdl/Publish.py
@@ -0,0 +1,17 @@
+from plugins.preflight.preflight_check import PreflightCheck
+from cgl.plugins.maya import lumbermill
+
+class Publish(PreflightCheck):
+
+    def getName(self):
+        pass
+
+    def run(self):
+        path_object = lumbermill.publish()
+        print path_object.path_root
+        self.pass_check('Version  Published!')
+        lumbermill.confirm_prompt(title='Publish Successful!', message='Version %s has been published!\n '
+                                                                       'you may close this scene' % path_object.version)
+
+
+
