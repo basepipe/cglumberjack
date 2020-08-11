abd633f0ae0f325a12c6267d2110238d5f0f3056
Tom <tmikota@gmail.com>
2020-08-11 10:03:13 -0400

    fixed collection import statement

--- cgl/plugins/blender/lumbermill.py
+++ cgl/plugins/blender/lumbermill.py
@@ -9,8 +9,6 @@ from cgl.apps.lumbermill.main import CGLumberjackWidget
 from cgl.plugins.blender.main_window import CGLumberjack
 import bpy
 
-
-
 CONFIG = app_config()
 PROJ_MANAGEMENT = CONFIG['account_info']['project_management']
 PADDING = CONFIG['default']['padding']
@@ -126,7 +124,7 @@ class LumberObject(PathObject):
         self.set_render_paths()
 
     def set_render_paths(self):
-        padding = '#'*self.frame_padding
+        padding = '#' * self.frame_padding
         if self.task == 'anim':
             render_path = self.copy(context='render', ext='jpg', set_proper_filename=True).path_root
             self.render_path = render_path.replace('.jpg', '.{}.jpg'.format(padding))
@@ -184,9 +182,14 @@ def version_up(vtype='minor'):
     create_file_dirs(new_version.path_root)
     return save_file_as(new_version.path_root)
 
-def import_file(filepath='', namespace=None, collection_name=None, append = True, linked=True ,type = 'COLLECTION'):
+
+def import_file(filepath='', namespace=None, collection_name=None, append=True, linked=True, type='COLLECTION'):
     """
     imports file into a scene.
+    :param type: 'COLLECTION' , 'GROUP', 'ANIM' , 'CAMERA'
+    :param linked:
+    :param append:
+    :param collection_name:
     :param filepath:
     :param namespace:
     :return:
@@ -203,16 +206,14 @@ def import_file(filepath='', namespace=None, collection_name=None, append = True
             collection_name = collection.asset
         # append, set to true to keep the link to the original file
 
-
         if type == 'COLLECTION':
             print('collection selected')
             with bpy.data.libraries.load(filepath, link=append) as (data_from, data_to):
                 data_to.collections = [c for c in data_from.collections if c.startswith(collection_name)]
                 # for obj in data_to.groups[0].objects:
                 #     bpy.context.scene.objects.link(obj)
-                if linked == True:
+                if linked:
                     obj = bpy.data.objects.new(collection_name, None)
-
                     obj.instance_type = 'COLLECTION'
                     obj.instance_collection = bpy.data.collections[collection_name]
                     bpy.context.collection.objects.link(obj)
@@ -226,23 +227,13 @@ def import_file(filepath='', namespace=None, collection_name=None, append = True
             print('anim Import selected')
             with bpy.data.libraries.load(filepath, link=linked) as (data_from, data_to):
                 data_to.actions = data_from.actions
+
         if type == 'CAMERA':
             print('Camera Import selected')
             with bpy.data.libraries.load(filepath, link=linked) as (data_from, data_to):
-                #data_to.cameras = [c for c in data_from.cameras if c.startswith(collection_name)]
+                # data_to.cameras = [c for c in data_from.cameras if c.startswith(collection_name)]
                 data_to.objects = [c for c in data_from.objects if c.startswith(collection_name)]
 
-
-        # link collection to scene collection
-
-
-        #for coll in data_to.collections:
-            #if coll is not None:
-                #bpy.data.scenes['Scene'].collection.children.link(coll)
-
-
-        # bpy.ops.wm.append(directory='{}/Collection'.format(filepath),
-        #                   filepath=filepath, filename=collection_name, link=True, instance_collections=True)
 
 def open_file(filepath):
     """
@@ -321,7 +312,7 @@ def export_selected(to_path):
         bpy.ops.export_scene.blend(filepath=to_path, use_selection=True)
 
 
-def create_turntable(length=250, task=False, startFrame =1):
+def create_turntable(length=250, task=False, startFrame=1):
     """
     Creates a Turntable of length around the selected object, or around a "task" object.
     This is specific to 3d applications.
@@ -330,18 +321,17 @@ def create_turntable(length=250, task=False, startFrame =1):
     :return:
     """
 
-
     selectedObject = bpy.context.object
     objectDimensions = selectedObject.dimensions
     distanceFromObject = objectDimensions[0] * -4
     height = objectDimensions[2] / 2
     endFrame = startFrame - 1 + length
-    #Creates locator top parent camera to
+    # Creates locator top parent camera to
     locator = bpy.data.objects.new('TurnTableLocator', None)
     locator.empty_display_size = 2
     locator.empty_display_type = 'PLAIN_AXES'
     bpy.context.scene.collection.objects.link(locator)
-    #Create camera
+    # Create camera
     turnTableCamObj = bpy.data.cameras.new('turnTable')
     turnTable = bpy.data.objects.new("TurnTableCam", turnTableCamObj)
     bpy.context.scene.collection.objects.link(turnTable)
@@ -349,7 +339,7 @@ def create_turntable(length=250, task=False, startFrame =1):
 
     turnTable.location = (0, distanceFromObject, height)
     turnTable.rotation_euler = (1.5707963705062866, 0.0, 0.0)
-    #Animates TurnTable
+    # Animates TurnTable
     locator.keyframe_insert("rotation_euler", frame=startFrame)
     locator.rotation_euler = (0, 0, 6.2831854820251465)
     locator.keyframe_insert("rotation_euler", frame=endFrame)
@@ -425,10 +415,10 @@ def render():
     renders the current scene.  Based on the task we can derive what kind of render and specific render settings.
     :return:
     """
-    previewRenderTypes = ['anim','rig','mdl']
+    previewRenderTypes = ['anim', 'rig', 'mdl']
     file_out = scene_object().render_path.split('#')[0]
 
-    if scene_object().task in previewRenderTypes :
+    if scene_object().task in previewRenderTypes:
         bpy.context.scene.render.image_settings.file_format = 'JPEG'
         # bpy.context.scene.render.ffmpeg.format = 'QUICKTIME'
         bpy.context.scene.render.filepath = file_out
@@ -448,7 +438,7 @@ def review():
     """
     from cgl.core.project import do_review
     padding = scene_object().frame_padding
-    render_files = glob.glob(scene_object().render_path.replace('#'*padding, '*'))
+    render_files = glob.glob(scene_object().render_path.replace('#' * padding, '*'))
     if render_files:
         path_object = LumberObject(scene_object().render_path)
         do_review(progress_bar=None, path_object=path_object)
