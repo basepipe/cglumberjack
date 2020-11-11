b48391d5b014c3ab69f875cbdcbfcb9cc5a4ebcf
Guillermo Molina <gmolinart@gmail.com>
2020-11-11 10:41:45 -0400

    basic lumbermill functions
    Signed-off-by: Guillermo Molina <gmolinart@gmail.com>

---
+++ cgl/plugins/houdini/lumbermill.py
@@ -0,0 +1,282 @@
+import glob
+import logging
+import os
+import hou
+import getpass
+import json
+from cgl.core.path import PathObject
+from cgl.core.config import app_config, UserConfig
+from cgl.apps.lumbermill.main import CGLumberjack, CGLumberjackWidget
+
+CONFIG = app_config()
+PROJ_MANAGEMENT = CONFIG['account_info']['project_management']
+PADDING = CONFIG['default']['padding']
+PROCESSING_METHOD = UserConfig().d['methodology']
+SOFTWARE = os.path.basename(os.path.dirname(__file__))
+
+class BrowserWidget(CGLumberjackWidget):
+    def __init__(self, parent=None, path=None,
+                 show_import=False):
+        super(BrowserWidget, self).__init__(parent=parent, path=path, show_import=show_import)
+
+    def open_clicked(self):
+        print('Opening: %s' % self.path_object.path_root)
+
+    def import_clicked(self):
+        for selection in self.source_selection:
+            base_, ext = os.path.splitext(selection)
+            import_file(selection, namespace=None)
+        self.parent().parent().accept()
+
+    def reference_clicked(self):
+        for selection in self.source_selection:
+            base_, ext = os.path.splitext(selection)
+            reference_file(selection, namespace=None)
+        self.parent().parent().accept()
+
+
+class AppMainWindow(CGLumberjack):
+    def __init__(self, parent=None, path=None, user_info=None):
+        CGLumberjack.__init__(self, parent, user_info=user_info, previous_path=path, sync_enabled=False)
+        print('Application Path path is %s' % path)
+        self.setCentralWidget(BrowserWidget(self, show_import=True, path=path))
+
+
+
+
+class LumberObject(PathObject):
+
+    def __init__(self, path_object=None):
+        if not path_object:
+            path_object = get_scene_name()
+        self.data = {}
+        self.root = CONFIG['paths']['root'].replace('\\', '/')
+        self.company = None
+        self.project = None
+        self.scope = None
+        self.context = None
+        self.seq = None
+        self.shot = None
+        self.type = None
+        self.asset = None
+        self.variant = None
+        self.user = None
+        self.version = None
+        self.major_version = None
+        self.minor_version = None
+        self.ext = None
+        self.filename = None
+        self.filename_base = None
+        self.resolution = None
+        self.frame = None
+        self.aov = None
+        self.render_pass = None
+        self.shotname = None
+        self.assetname = None
+        self.task = None
+        self.camera = None
+        self.file_type = None
+        self.frame_padding = CONFIG['default']['padding']
+        self.scope_list = CONFIG['rules']['scope_list']
+        self.context_list = CONFIG['rules']['context_list']
+        self.path = None  # string of the properly formatted path
+        self.path_root = None  # this gives the full path with the root
+        self.thumb_path = None
+        self.playblast_path = None
+        self.render_path = None
+        self.preview_path = None
+        self.preview_seq = None
+        self.hd_proxy_path = None
+        self.start_frame = None
+        self.end_frame = None
+        self.frame_rate = None
+        self.frame_range = None
+        self.template = []
+        self.filename_template = []
+        self.actual_resolution = None
+        self.date_created = None
+        self.date_modified = None
+        self.project_config = None
+        self.company_config = None
+        self.software_config = None
+        self.asset_json = None
+        self.shot_json = None
+        self.task_json = None
+        self.command_base = ''
+        self.project_json = None
+        self.status = None
+        self.due = None
+        self.assigned = None
+        self.priority = None
+        self.ingest_source = '*'
+        self.processing_method = PROCESSING_METHOD
+        self.proxy_resolution = '1920x1080'
+        self.path_template = []
+        self.version_template = []
+
+        try:
+            if isinstance(path_object, unicode):
+                path_object = str(path_object)
+        except NameError:
+            pass
+        if isinstance(path_object, dict):
+            self.process_dict(path_object)
+        elif isinstance(path_object, str):
+            self.process_string(path_object)
+        elif isinstance(path_object, PathObject):
+            self.process_dict(path_object.data)
+        else:
+            logging.error('type: %s not expected' % type(path_object))
+        self.set_render_paths()
+
+    def set_render_paths(self):
+        padding = '#' * self.frame_padding
+        previewRenderTypes = ['anim', 'rig', 'mdl', 'lay', 'remsh', 'grmnt']
+
+        if self.task in previewRenderTypes:
+            render_path = self.copy(context='render', ext='jpg', set_proper_filename=True).path_root
+            self.render_path = render_path.replace('.jpg', '.{}.jpg'.format(padding))
+        else:
+            render_path = self.copy(context='render', ext='exr', set_proper_filename=True).path_root
+            self.render_path = render_path.replace('.exr', '.{}.exr'.format(padding))
+
+def get_scene_name():
+    """
+    get current scene name
+    :return:
+    """
+
+    return hou.hipFile.path()
+
+
+
+def scene_object():
+    """
+    returns LumberObject of curent scene
+    :return:
+    """
+    return LumberObject(get_scene_name())
+
+
+def open_file(filepath):
+    """
+    Open File: filepath
+    :param filepath:
+    :return:
+    """
+    hou.hipFile.load(filepath=filepath)
+    return filepath
+
+def import_file(filepath=''):
+    """
+    imports file into a scene.
+    :param filepath:
+    :return:
+    """
+
+    if filepath.endswith('fbx'):
+        hou.hipFile.importFBX(filepath)
+    elif filepath.endswith('hip'):
+        hou.Node.loadItemsFromFile()
+
+
+def reference_file(filepath='', namespace=None):
+    """
+    creates a "reference" of a file, this is a convenience function to be used in various software plugins
+    to promote continuity accross plugins
+    :param namespace:
+    :param filepath:
+    :return:
+    """
+    print(filepath)
+    pass
+
+def save_file(filepath=''):
+    """
+    Save Current File
+    :param filepath:
+    :return:
+    """
+    return hou.hipFile.save()
+
+def save_file_as(filepath):
+    """
+    save current file as
+    :param filepath:
+    :return:
+    """
+    hou.hipFile.save(filepath)
+    return filepath
+
+
+def version_up(vtype='minor'):
+    """
+    versions up the current scene
+    :param vtype: minor or major
+    :return:
+    """
+    path_object = LumberObject(get_scene_name())
+    if vtype == 'minor':
+        new_version = path_object.new_minor_version_object()
+    elif vtype == 'major':
+        new_version = path_object.next_major_version()
+    create_file_dirs(new_version.path_root)
+    return save_file_as(new_version.path_root)
+
+def create_file_dirs(file_path):
+    """
+    given file_path checks to see if directories exist and creates them if they don't.
+    :param file_path: path to file you're about to create.
+    :return:
+    """
+    dirname = os.path.dirname(file_path)
+    if os.path.isdir(file_path):
+        dirname = file_path
+    print(dirname)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+
+def current_user():
+    """
+    find the currently logged in user
+    Returns:
+        str: username
+
+    """
+    return getpass.getuser().lower()
+
+def review():
+    from cgl.core.project import do_review
+    playblast_seq = scene_object().copy(context='render', filename='playblast.####.jpg')
+    if glob.glob(playblast_seq.path_root.replace('####', '*')):
+        print('exists - reviewing')
+        do_review(progress_bar=None, path_object=playblast_seq)
+
+
+def launch_():
+    scene_name = get_scene_name()
+    scene = LumberObject(scene_name)
+    location = '%s/*' % scene.split_after('shot')
+    project_management = CONFIG['account_info']['project_management']
+    users = CONFIG['project_management'][project_management]['users']
+    if current_user() in users:
+        user_info = users[current_user()]
+        app = QtWidgets.QApplication.instance()
+        main_window = AppMainWindow(user_info=user_info, path=location)
+        main_window.setWindowTitle('Lumbermill: Maya')
+        main_window.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        main_window.show()
+        main_window.raise_()
+    app.exec_()
+
+
+
+""""
+
+publish()
+render()
+export_selected()
+launch_preflight()
+launch_()
+"""
